027b2c9d77aec1ca4898e317907aa13305d14a6c
卢冉 <luran@lurandebijibendiannao.local>
2025-12-16 14:02:21 +0100

    Ran LU solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -159,6 +159,7 @@ class MonthlySplit(BaseCrossValidator):
 
     def get_n_splits(self, X, y=None, groups=None):
         """Return the number of splitting iterations in the cross-validator.
+        
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
@@ -193,6 +194,7 @@ class MonthlySplit(BaseCrossValidator):
 
     def split(self, X, y, groups=None):
         """Generate indices to split data into training and test set.
+
         Parameters
         ----------
         X : array-like of shape (n_samples, n_features)
